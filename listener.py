c9fa4df0f0f0c6a95d909b415f9529e8b44fe36a
Nami Shah <shah.nami@hotmail.com>
2019-11-28 16:51:06 +0000

    print messages

--- listener.py
+++ listener.py
@@ -3,25 +3,25 @@ from github_webhook import Webhook
 from flask import Flask
 
 app = Flask(__name__)  # Standard Flask app
-webhook = Webhook(app)  # Defines '/postreceive' endpoint
+# Defines '/postreceive' endpoint
+webhook = Webhook(app, secret="AWeU4q9FpR03")
 
 
 @app.route("/")        # Standard Flask endpoint
 def hello_world():
-    return "Hello, World!"
+    return "-"
 
 
-@webhook.hook()        # Defines a handler for the 'push' event
+@webhook.hook("push")        # Defines a handler for the 'push' event
 def on_push(data):
     print("[-] Got push with: {0}".format(data))
-    # old_path = os.popen('pwd').read()
-    # print(f"[-] Changing directory from: {old_path}")
-    # change_dir = os.popen('cd /code').read()
-    # print(f"[-] Changing directory: {change_dir}")
-    # new_path = os.popen('pwd').read()
-    # print(f"[-] Changed directory to: {new_path}")
-    # git_pulled = os.popen("git pull").read()
-    # print(f"[-] Performed `git pull`: {git_pulled}")
+    old_path = os.popen('pwd').read()
+    print(f"[-] Changing directory from: {old_path}")
+    os.chdir('/code')
+    new_path = os.popen('pwd').read()
+    print(f"[-] Changed directory to: {new_path}")
+    git_pulled = os.popen("git pull").read()
+    print(f"[-] Performed `git pull`: {git_pulled}")
 
 
 if __name__ == "__main__":
